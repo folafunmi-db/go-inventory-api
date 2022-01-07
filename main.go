a3209f410caeeec424e4b50f29e55b4d5e07c879
Folafunmi <folafunmimustapha@gmail.com>
2022-01-07 01:59:39 +0100

    feat: add inventory api router 🎉

--- main.go
+++ main.go
@@ -1,36 +1,55 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
-	"encoding/json"
 	"log"
 	"net/http"
 )
 
 type Item struct {
-	UID string `json: "UID"`
-	Name string `json: "Name"`
-	Desc string `json: "Desc"`
+	UID   string  `json: "UID"`
+	Name  string  `json: "Name"`
+	Desc  string  `json: "Desc"`
 	Price float64 `json: "Price"`
-
 }
 
 // global variable
-var Inventory []Item
+var inventory []Item
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Endpoint called: homepage()")
+}
+
+func getInventory(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Println("Function called: inventory()")
+
+	json.NewEncoder(w).Encode(inventory)
 }
 
 func handleRequests() {
 	router := mux.NewRouter().StrictSlash(true)
 
 	router.HandleFunc("/", homePage).Methods("GET")
+	router.HandleFunc("/inventory", getInventory).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
 
 func main() {
+	inventory = append(inventory, Item{
+		UID:   "0",
+		Name:  "Cheese",
+		Desc:  "A fine block of cheese",
+		Price: 5,
+	})
+	inventory = append(inventory, Item{
+		UID:   "0",
+		Name:  "Milk",
+		Desc:  "A jug of milk",
+		Price: 15,
+	})
 	handleRequests()
 }
